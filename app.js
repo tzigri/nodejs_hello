178c5de9f2a9ab3263b2ab0c3347857bb438f121
tzigri <tzihgri@gmail.com>
2022-12-04 10:30:56 +0200

    changed start up

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const port = process.env.port || 4000;
 
 http.createServer(function (req, res) {
   res.writeHead(200, {'Content-Type': 'text/html'});
-  res.end('Hello for Node JS');
+  res.end('Hello');
 }).listen(port);
 
 console.log("Running at port " + port)
