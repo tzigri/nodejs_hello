a613d4a9423478cad987a918278cddcbf9d6ec9b
tzigri <tzihgri@gmail.com>
2022-12-04 10:19:56 +0200

    Changed port

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 const http = require('http');
-const port = 4000;
+const port = process.env.port || 4000;
 
 http.createServer(function (req, res) {
   res.writeHead(200, {'Content-Type': 'text/html'});
